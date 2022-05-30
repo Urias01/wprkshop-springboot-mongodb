5ee8557795e5b43c87bbcad6f2ed53e1570be3b3
Urias01 <uriasluis64@gmail.com>
2022-05-30 17:51:38 -0300

    consultas simples com query methods

--- src/main/java/com/uriasluis/workshopmongo/resources/PostResource.java
+++ src/main/java/com/uriasluis/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.uriasluis.workshopmongo.resources;
 import com.uriasluis.workshopmongo.domain.Post;
 import com.uriasluis.workshopmongo.domain.User;
 import com.uriasluis.workshopmongo.dto.UserDTO;
+import com.uriasluis.workshopmongo.resources.util.URL;
 import com.uriasluis.workshopmongo.services.PostService;
 import com.uriasluis.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,15 @@ public class PostResource {
         Post obj = service.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    // @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
     }
 
 }
